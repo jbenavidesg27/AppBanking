4d8e185f88703aee26e5195e47fc7bdaa8c4577e
mmelgar1993 <84425650+mmelgar1993@users.noreply.github.com>
2022-07-25 12:10:17 -0500

    subiendo

--- workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
+++ workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
@@ -1,10 +1,28 @@
 package com.nttdata.bootcamp.active.model;
 
+<<<<<<< HEAD
+=======
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+>>>>>>> parent of fe8af3a (Active)
 import lombok.Data;
 
 @Data
+<<<<<<< HEAD
 public class Active {
 	
+=======
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "actives")
+public class Active {
+	
+	@Id
+	private Integer id;
+	private String descripcion;
+>>>>>>> parent of fe8af3a (Active)
 	
 
 }
