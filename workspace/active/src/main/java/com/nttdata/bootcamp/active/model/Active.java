878bd1f15c0b0453ec59210e6d61dc7eaaa7e88b
mmelgar1993 <84425650+mmelgar1993@users.noreply.github.com>
2022-07-25 12:20:04 -0500

    merge

--- workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
+++ workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
@@ -1,30 +1,21 @@
 package com.nttdata.bootcamp.active.model;
 
-<<<<<<< HEAD
-=======
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
->>>>>>> parent of fe8af3a (Active)
+
 import lombok.Data;
 
 @Data
-<<<<<<< HEAD
-public class Active {
-	
-=======
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "actives")
 public class Active {
 	
 	@Id
-<<<<<<< HEAD
-	private Integer id;
-	private String descripcion;
->>>>>>> parent of fe8af3a (Active)
-=======
+
 	private String id;
 	
 	private Credit credit;
@@ -36,7 +27,7 @@ public class Active {
 	private Date creditDate;
 	
 	private String period;
->>>>>>> dev
+
 	
 
 }
