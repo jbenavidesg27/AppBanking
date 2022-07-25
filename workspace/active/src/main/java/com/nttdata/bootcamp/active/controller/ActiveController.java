05801fa6646780c5e8986e4ca7a9ec02b1e3ed8d
mmelgar1993 <84425650+mmelgar1993@users.noreply.github.com>
2022-07-25 12:42:04 -0500

    Subiendo cambios de clases
    estructurando las clases faltante

--- workspace/active/src/main/java/com/nttdata/bootcamp/active/controller/ActiveController.java
+++ workspace/active/src/main/java/com/nttdata/bootcamp/active/controller/ActiveController.java
@@ -1,19 +1,32 @@
 package com.nttdata.bootcamp.active.controller;
 
-import java.util.Arrays;
-import java.util.List;
+import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nttdata.bootcamp.active.model.Active;
+import com.nttdata.bootcamp.active.service.IActiveService;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/active")
 public class ActiveController {
+	
+	@Autowired
+	IActiveService activeService;
 	
 	/*Peticiones Rest */
 	
--- workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
+++ workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
@@ -1,12 +1,16 @@
 package com.nttdata.bootcamp.active.model;
 
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
