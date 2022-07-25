f2b275f91a8f6408c2b72f3321d19b183f67872b
jbenavidesg27 <jbenavidesg27@gmail.com>
2022-07-25 11:35:22 -0500

    Active

--- workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
+++ workspace/active/src/main/java/com/nttdata/bootcamp/active/model/Active.java
@@ -1,9 +1,23 @@
 package com.nttdata.bootcamp.active.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "person")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Active {
+	
+	@Id
+	private String id;
 	
 	
 
